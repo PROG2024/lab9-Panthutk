0f3e9bba93d47f7cfecbedf3837f1dd425cf4a2b
Panthut Ketphan <panthat.k@gmail.com>
2024-03-18 21:30:00 +0700

    done the test_counter with 3 test initial , increment and singleton

--- counter/test_counter.py
+++ counter/test_counter.py
@@ -9,3 +9,23 @@
 """
 import unittest
 from counter import Counter
+
+
+class TestCounter(unittest.TestCase):
+    def test_initial_count(self):
+        counter = Counter()
+        self.assertEqual(counter.count, 0)  # initial count should be 0
+
+    def test_increment(self):
+        counter = Counter()
+        counter.increment()
+        self.assertEqual(counter.count, 1)  # should be 1
+
+    def test_singleton(self):
+        counter1 = Counter()
+        counter2 = Counter()
+        self.assertIs(counter1, counter2)  # same instance
+
+
+if __name__ == "__main__":
+    unittest.main()
